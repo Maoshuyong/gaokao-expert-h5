846fef0fb3abe1dd5a9f6065042c50eaa35f332a
fengweitao <80403250@qq.com>
2026-04-09 01:41:49 +0800

    切换到ngrok隧道

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ const API_CONFIG = isLocal ? {
     token: '1654d4a2d9b7f9a8ba9551897f5321d1e56e8f1812c78703'
 } : {
     // 公网模式：需要配置完整的API地址
-    baseURL: 'https://7e1305f09a9feb.lhr.life',
+    baseURL: 'https://psammophytic-unprofessional-jeni.ngrok-free.dev',
     token: '1654d4a2d9b7f9a8ba9551897f5321d1e56e8f1812c78703'
 };
 
