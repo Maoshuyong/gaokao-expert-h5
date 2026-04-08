9391692ab56870e32e456424493e764414573661
fengweitao <80403250@qq.com>
2026-04-08 22:56:50 +0800

    更新API地址: aabb45caaf547b

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ const API_CONFIG = isLocal ? {
     token: '1654d4a2d9b7f9a8ba9551897f5321d1e56e8f1812c78703'
 } : {
     // 公网模式：需要配置完整的API地址
-    baseURL: 'https://f3c7345f1f499f.lhr.life',
+    baseURL: 'https://aabb45caaf547b.lhr.life',
     token: '1654d4a2d9b7f9a8ba9551897f5321d1e56e8f1812c78703'
 };
 
