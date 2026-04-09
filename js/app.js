11e833a4755afeececcd7841bce4ea86d55001ee
fengweitao <80403250@qq.com>
2026-04-09 20:59:27 +0800

    切换到Cloudflare Tunnel

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ const API_CONFIG = isLocal ? {
     token: '1654d4a2d9b7f9a8ba9551897f5321d1e56e8f1812c78703'
 } : {
     // 公网模式：需要配置完整的API地址
-    baseURL: 'https://psammophytic-unprofessional-jeni.ngrok-free.dev',
+    baseURL: 'https://really-medicaid-karma-seeing.trycloudflare.com',
     token: '1654d4a2d9b7f9a8ba9551897f5321d1e56e8f1812c78703'
 };
 
