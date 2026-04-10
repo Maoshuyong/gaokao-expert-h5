ebd0d3010f45b551ee3120e61541453c34cef731
fengweitao <80403250@qq.com>
2026-04-10 08:40:21 +0800

    自动更新隧道: https://e10693fe3c2359.lhr.lif

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ const API_CONFIG = isLocal ? {
     token: '1654d4a2d9b7f9a8ba9551897f5321d1e56e8f1812c78703'
 } : {
     // 公网模式：需要配置完整的API地址
-    baseURL: 'https://b6b35c8ee188c5.lhr.life',
+    baseURL: 'https://e10693fe3c2359.lhr.life',
     token: '1654d4a2d9b7f9a8ba9551897f5321d1e56e8f1812c78703'
 };
 
